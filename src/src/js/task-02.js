06c9ad0a65053ca25d7a3cad2349b9f768cdee75
Sergey Donyk <doncarlos@i.ua>
2023-02-07 00:40:32 +0200

    Update task-02.js

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+
+// const container = document.querySelector('#main-container');
+// const ulEl = document.createElement('ul');
+
+//  const techElements = technologies
+//   .map((el) => `<li>${el}</li>`)
+//   .join('');
+
+ingredients.forEach((ingredientsItem) => {
+  //  const liEl = document.createElement('li');
+  // liEl.textContent = ingredientsItem;
+  //  ingredients.append(liEl)
+
+  //  const liEl = `<li>${ingredientsItem}</li>`;
+  //  ingredients.insertAdjacentHTML('afterbegin', liEl);
+});
+
+ const itemIngredients = ingredients.reduce(
+  (acum, el) => (acum += `<li>${el}</li>`),
+  ''
+ );
+
+ ingredients.innerHTML = itemIngredients;
+
+document.body.append(ingredients);
